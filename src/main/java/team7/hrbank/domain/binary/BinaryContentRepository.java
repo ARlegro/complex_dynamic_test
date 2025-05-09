05ed015981711f601dd056c532e3563796e4a3ec
ARlegro <icb1696@naver.com>
2025-05-09 11:30:53 +0900

    Delete src/main/java/team7/hrbank/domain/binary directory

--- src/main/java/team7/hrbank/domain/binary/BinaryContentRepository.java
+++
@@ -1,6 +0,0 @@
-package team7.hrbank.domain.binary;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BinaryContentRepository extends JpaRepository<BinaryContent, Long> {
-}
